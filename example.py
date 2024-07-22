debe971437f2e0813c66326abf48eea8a11976ee
Rumiantsau Uladzimir Work <rumiantsau88@gmail.com>
2024-07-22 16:31:44 +0500

    add files

---
+++ example.py
@@ -0,0 +1,9 @@
+my_dict = {'my_key': "afsdfsd",
+           'your_key': 'jhskfssd',
+           'their_key': 'Khsdyu67u26&'
+           }
+print(my_dict['my_key'])
+
+my_list = [['first', 1], ['second', 2], ['third', 3]]
+my_second_dict = dict(my_list)
+print(my_second_dict)
